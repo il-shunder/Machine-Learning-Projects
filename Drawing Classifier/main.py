149ed09abfa04d268816745c7c1db25f0e1b662e
Illia Shunder <shunder.work@gmail.com>
2025-03-03 19:22:45 +0200

    create logic to select the model, create logic to remove training data

--- Drawing Classifier/main.py
+++ Drawing Classifier/main.py
@@ -1,10 +1,16 @@
 import os
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import simpledialog, ttk
 
 import PIL
 import PIL.Image
 import PIL.ImageDraw
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
 
 
 class DrawingClassifier:
@@ -12,7 +18,7 @@ class DrawingClassifier:
     CANVAS_HEIGHT = 500
     N_CLASSES = 3
     BRUSH_WIDTH = 15
-    TEMP_IMAGE = "image.png"
+    TEMP_IMAGE_PATH = "image.png"
     TRAIN_IMAGE_SIZE = (50, 50)
 
     def __init__(self, root=tk.Tk(), root_title="Drawing Classifier"):
@@ -24,7 +30,15 @@ class DrawingClassifier:
 
         self.counters = [0] * self.N_CLASSES
 
-        self.modal = None
+        self.model = LinearSVC()
+        self.model_options = [
+            type(self.model).__name__,
+            type(RandomForestClassifier()).__name__,
+            type(LogisticRegression()).__name__,
+            type(GaussianNB()).__name__,
+            type(KNeighborsClassifier()).__name__,
+            type(DecisionTreeClassifier()).__name__,
+        ]
         self.image, self.draw = None, None
 
         self.init_gui()
@@ -57,8 +71,10 @@ class DrawingClassifier:
             )
             self.btns_for_classes[i].grid(row=0, column=i, sticky=tk.NSEW)
 
-        change_btn = tk.Button(btn_frame, text="Change Model", command=self.change_model)
-        change_btn.grid(row=1, column=0, sticky=tk.NSEW)
+        self.model_dropdown = ttk.Combobox(btn_frame, state="readonly", values=self.model_options)
+        self.model_dropdown.current(0)
+        self.model_dropdown.grid(row=1, column=0, sticky=tk.NSEW)
+        self.model_dropdown.bind("<<ComboboxSelected>>", self.change_model)
 
         train_btn = tk.Button(btn_frame, text="Train Model", command=self.train_model)
         train_btn.grid(row=1, column=1, sticky=tk.NSEW)
@@ -69,11 +85,11 @@ class DrawingClassifier:
         predict_btn = tk.Button(btn_frame, text="Predict", command=self.predict)
         predict_btn.grid(row=2, columnspan=3, sticky=tk.NSEW)
 
-        self.status_label = tk.Label(btn_frame, text=f"Current Model: {type(self.modal).__name__}")
+        self.status_label = tk.Label(btn_frame, text=f"Current Model: {type(self.model).__name__}")
         self.status_label.config(font=("Arial", 20))
         self.status_label.grid(row=4, column=1, sticky=tk.NSEW)
 
-        # self.root.protocol("WM_DELETE_WINDOW", self.on_delete)
+        self.root.protocol("WM_DELETE_WINDOW", self.on_delete)
         self.root.attributes("-topmost", True)
         self.root.mainloop()
 
@@ -92,15 +108,15 @@ class DrawingClassifier:
         if not os.path.isdir(str(index)):
             os.mkdir(str(index))
 
-        self.image.save(self.TEMP_IMAGE)
+        self.image.save(self.TEMP_IMAGE_PATH)
 
         filepath = f"{index}/image{self.counters[index]}.png"
-        img = PIL.Image.open(self.TEMP_IMAGE)
+        img = PIL.Image.open(self.TEMP_IMAGE_PATH)
         img.thumbnail(self.TRAIN_IMAGE_SIZE, PIL.Image.Resampling.LANCZOS)
         img.save(filepath)
 
-        if os.path.isfile(self.TEMP_IMAGE):
-            os.remove(self.TEMP_IMAGE)
+        if os.path.isfile(self.TEMP_IMAGE_PATH):
+            os.remove(self.TEMP_IMAGE_PATH)
 
         self.counters[index] += 1
         self.clear()
@@ -112,14 +128,42 @@ class DrawingClassifier:
     def train_model(self):
         pass
 
-    def change_model(self):
-        pass
+    def change_model(self, dropdown):
+        dropdown = self.model_dropdown.get()
+
+        if dropdown == "RandomForestClassifier":
+            self.set_model(RandomForestClassifier())
+        elif dropdown == "LogisticRegression":
+            self.set_model(LogisticRegression())
+        elif dropdown == "GaussianNB":
+            self.set_model(GaussianNB())
+        elif dropdown == "KNeighborsClassifier":
+            self.set_model(KNeighborsClassifier())
+        elif dropdown == "DecisionTreeClassifier":
+            self.set_model(DecisionTreeClassifier())
+        else:
+            self.set_model(LinearSVC())
+
+    def set_model(self, model):
+        self.model = model
 
     def predict(self):
         pass
 
     def on_delete(self):
-        pass
+        self.remove_training_data()
+        self.root.destroy()
+        exit()
+
+    def remove_training_data(self):
+        for dir in range(len(self.counters)):
+            dir = str(dir)
+            if os.path.isdir(dir):
+                for file in os.listdir(dir):
+                    file_path = os.path.join(dir, file)
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+                os.rmdir(dir)
 
 
 DrawingClassifier()
