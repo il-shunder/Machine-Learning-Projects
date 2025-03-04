5788a8d44f5b086d5b1bc96a01a5381871aef5bd
Illia Shunder <shunder.work@gmail.com>
2025-03-04 19:42:18 +0200

    create logic to train the model, create logic to make predictions, adjust the width of the columns

--- Drawing Classifier/main.py
+++ Drawing Classifier/main.py
@@ -2,6 +2,8 @@ import os
 import tkinter as tk
 from tkinter import simpledialog, ttk
 
+import cv2 as cv
+import numpy as np
 import PIL
 import PIL.Image
 import PIL.ImageDraw
@@ -45,7 +47,7 @@ class DrawingClassifier:
 
     def init_gui(self):
         self.canvas = tk.Canvas(self.root, width=self.CANVAS_WIDTH - 10, height=self.CANVAS_HEIGHT - 10, bg="white")
-        self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
+        self.canvas.pack()
         self.canvas.bind("<B1-Motion>", self.paint)
 
         self.image = PIL.Image.new("RGB", (self.CANVAS_WIDTH, self.CANVAS_HEIGHT), (255, 255, 255))
@@ -59,9 +61,9 @@ class DrawingClassifier:
         btn_frame = tk.Frame(self.root)
         btn_frame.pack(fill=tk.X, side=tk.BOTTOM)
 
-        btn_frame.columnconfigure(0, weight=1)
-        btn_frame.columnconfigure(1, weight=1)
-        btn_frame.columnconfigure(2, weight=1)
+        btn_frame.grid_columnconfigure(0, weight=1, uniform="buttons")
+        btn_frame.grid_columnconfigure(1, weight=1, uniform="buttons")
+        btn_frame.grid_columnconfigure(2, weight=1, uniform="buttons")
 
         for i in range(self.N_CLASSES):
             self.btns_for_classes[i] = tk.Button(
@@ -85,9 +87,9 @@ class DrawingClassifier:
         predict_btn = tk.Button(btn_frame, text="Predict", command=self.predict)
         predict_btn.grid(row=2, columnspan=3, sticky=tk.NSEW)
 
-        self.status_label = tk.Label(btn_frame, text=f"Current Model: {type(self.model).__name__}")
-        self.status_label.config(font=("Arial", 20))
-        self.status_label.grid(row=4, column=1, sticky=tk.NSEW)
+        self.predicted_class = tk.Label(btn_frame, text="There is no predicted class")
+        self.predicted_class.config(font=("Arial", 20))
+        self.predicted_class.grid(row=4, columnspan=3, sticky=tk.NSEW)
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_delete)
         self.root.attributes("-topmost", True)
@@ -115,9 +117,6 @@ class DrawingClassifier:
         img.thumbnail(self.TRAIN_IMAGE_SIZE, PIL.Image.Resampling.LANCZOS)
         img.save(filepath)
 
-        if os.path.isfile(self.TEMP_IMAGE_PATH):
-            os.remove(self.TEMP_IMAGE_PATH)
-
         self.counters[index] += 1
         self.clear()
 
@@ -126,7 +125,25 @@ class DrawingClassifier:
         self.draw.rectangle([0, 0, 1000, 1000], fill="white", outline="white")
 
     def train_model(self):
-        pass
+        X_train, y_train = self.get_training_data()
+        self.model.fit(X_train, y_train)
+        print("Model successfully trained!")
+
+    def get_training_data(self):
+        img_list = class_list = np.array([])
+        n_training_examples = sum(self.counters)
+
+        if n_training_examples > 0:
+            img_shape = self.TRAIN_IMAGE_SIZE[0] * self.TRAIN_IMAGE_SIZE[1]
+            for i in range(len(self.counters)):
+                for j in range(self.counters[i]):
+                    img = cv.imread(f"{i}/image{j}.png")[:, :, 0]
+                    img = img.reshape(img_shape)
+                    img_list = np.append(img_list, [img])
+                    class_list = np.append(class_list, i)
+            img_list = img_list.reshape(n_training_examples, img_shape)
+
+        return img_list, class_list
 
     def change_model(self, dropdown):
         dropdown = self.model_dropdown.get()
@@ -148,9 +165,22 @@ class DrawingClassifier:
         self.model = model
 
     def predict(self):
-        pass
+        self.image.save(self.TEMP_IMAGE_PATH)
+        img = PIL.Image.open(self.TEMP_IMAGE_PATH)
+        img.thumbnail(self.TRAIN_IMAGE_SIZE, PIL.Image.Resampling.LANCZOS)
+        img.save(self.TEMP_IMAGE_PATH)
+
+        img = cv.imread(self.TEMP_IMAGE_PATH)[:, :, 0]
+        img = img.reshape(self.TRAIN_IMAGE_SIZE[0] * self.TRAIN_IMAGE_SIZE[1])
+        prediction = self.model.predict([img])
+
+        self.predicted_class.config(text=f"Predicted Class: {self.classes[prediction[0]]}")
+
+        self.clear()
 
     def on_delete(self):
+        if os.path.isfile(self.TEMP_IMAGE_PATH):
+            os.remove(self.TEMP_IMAGE_PATH)
         self.remove_training_data()
         self.root.destroy()
         exit()
