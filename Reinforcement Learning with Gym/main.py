5713e2e2a1b79df93762dddbbb0985ed88e91736
Illia Shunder <shunder.work@gmail.com>
2025-03-12 20:33:51 +0200

    make gym work with random choices

--- Reinforcement Learning with Gym/main.py
+++ Reinforcement Learning with Gym/main.py
@@ -1,1 +1,22 @@
+import random
+
 import gym
+
+env = gym.make("CartPole-v1", render_mode="human")
+
+steps = 10
+
+for step in range(1, steps+1):
+    state = env.reset()
+    done = False
+    score = 0
+
+    while not done:
+        action = random.choice([0, 1])
+        _, reward, done, _ = env.step(action)
+        score += reward
+        env.render()
+
+    print(f"Step: {step}, score: {score}")
+
+env.close()
