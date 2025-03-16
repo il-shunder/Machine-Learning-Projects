dd395ba890cf8809fdb0352eb82c38e12807fee7
Illia Shunder <shunder.work@gmail.com>
2025-03-16 20:42:01 +0200

    add DQNAgent and logic to train and test the model

--- Gym CartPole-v1/main.py
+++ Gym CartPole-v1/main.py
@@ -1,14 +1,12 @@
-import random
-
 import gym
 import numpy as np
 import tensorflow as tf
-from keras import layers, models
+from keras import layers, models, optimizers
 from rl.agents import DQNAgent
 from rl.memory import SequentialMemory
 from rl.policy import BoltzmannQPolicy
 
-env = gym.make("CartPole-v1", render_mode="human")
+env = gym.make("CartPole-v1")
 
 states = env.observation_space.shape[0]
 actions = env.action_space.n
@@ -20,5 +18,21 @@ model = models.Sequential([
     layers.Dense(24, activation="relu"),
     layers.Dense(actions, activation="relu"),
 ])
+
+agent = DQNAgent(
+    model=model,
+    memory=SequentialMemory(limit=50000, window_length=1),
+    policy=BoltzmannQPolicy(),
+    nb_actions=actions,
+    nb_steps_warmup=10,
+    target_model_update=0.01
+)
+
+agent.compile(optimizers.Adam(0.001), metrics=["mae"])
+agent.fit(env, nb_steps=10000, visualize=False, verbose=1)
+
+results = agent.test(env, nb_episodes=10, visualize=True)
+
+print(np.mean(results.history["episode_reward"]))
 
 env.close()
