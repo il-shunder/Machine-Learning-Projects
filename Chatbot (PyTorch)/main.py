c6be525bac7acb636ba6062ec74d5af131c5693b
Illia Shunder <shunder.work@gmail.com>
2025-04-26 23:13:15 +0300

    create Chatbot Model

--- Chatbot (PyTorch)/main.py
+++ Chatbot (PyTorch)/main.py
@@ -1,6 +1,7 @@
 import json
 import os
 import random
+from turtle import forward
 
 import nltk
 import numpy as np
@@ -9,3 +10,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
+
+# nltk.download("punkt_tab")
+
+
+class ChatbotModel(nn.Module):
+    def __init__(self, input_size, output_size):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, output_size)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0, 5)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
