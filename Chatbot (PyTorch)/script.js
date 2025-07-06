39baa68649021a715d78741db0caab9010365afe
Illia Shunder <shunder.work@gmail.com>
2025-07-06 15:57:13 +0300

    remove redundant text from the messages

--- Chatbot (PyTorch)/script.js
+++ Chatbot (PyTorch)/script.js
@@ -4,8 +4,8 @@ window.parent.document.displayMessages = function (message, answer) {
     if (messagesBlock) {
         const messageElement = window.parent.document.createElement("div");
         messageElement.innerHTML = `
-            <div class="user-message">You: ${message}</div>
-            <div class="chatbot-message">Chatbot: ${answer}</div>
+            <div class="user-message">${message}</div>
+            <div class="chatbot-message">${answer}</div>
         `;
         
         messagesBlock.appendChild(messageElement);
